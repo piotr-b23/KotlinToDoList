89b038f457758325c5b0b5640ae67a196c66bacd
Piotr <Piotr@DESKTOP-VH1N9IR>
2021-03-02 19:47:09 +0100

    database stores data

--- app/src/main/java/com/example/kotlintodolist/AddTask.kt
+++ app/src/main/java/com/example/kotlintodolist/AddTask.kt
@@ -3,26 +3,21 @@ package com.example.kotlintodolist
 import android.app.DatePickerDialog
 import android.os.Build
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.annotation.RequiresApi
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlintodolist.ToDoDatabase.Companion.toDoDatabase
 import java.util.*
 
 class AddTask : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
 
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
@@ -30,9 +25,18 @@ class AddTask : AppCompatActivity() {
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
         val pickDateButton: Button = findViewById(R.id.pickDateButton)
+        val addTaskButton: Button = findViewById(R.id.addTaskButton)
         val dateTextView: TextView = findViewById(R.id.textView2)
+        val description: EditText = findViewById(R.id.taskDescription)
 
         val spinner: Spinner = findViewById(R.id.TaskPrioritySpinner)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+
+        toDoDatabase = ToDoDatabase.getInstance(this)
+
+
         ArrayAdapter.createFromResource(
                 this,
                 R.array.priority,
@@ -51,6 +55,16 @@ class AddTask : AppCompatActivity() {
             dpd.datePicker.minDate = System.currentTimeMillis() - 1000
             dpd.show()
         }
+
+        addTaskButton.setOnClickListener {
+            val task = ToDoEntity(description.text.toString(),spinner.selectedItem.toString())
+            task.detail = "test"
+            toDoDatabase!!.getDAO().addNewTask(task)
+            finish()
+        }
+
+
+
 
 
     }
